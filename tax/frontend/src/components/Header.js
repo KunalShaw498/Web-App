6cf60ec9f6fd9a5498f1d857b5f47bcb145bdcb2
Kunal Shaw <78487132+KunalShaw498@users.noreply.github.com>
2022-02-04 09:12:35 +0530

    commit

--- tax/frontend/src/components/Header.js
+++ tax/frontend/src/components/Header.js
@@ -25,7 +25,7 @@ function Header() {
   //     dispatch(signout());
   //   };
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+    <header className="fixed top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <a href="/">
